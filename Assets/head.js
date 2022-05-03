d14348e35f8d7ec944adf3037d12b061a5e0a162
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-05-03 10:36:50 +0200

    x

---
+++ Assets/head.js
@@ -0,0 +1,7 @@
+document.write('\
+\
+    <link rel="stylesheet" href="Style.css">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <link rel="icon" type="image/x-icon" href="favicon.ico">\
+\
+');
