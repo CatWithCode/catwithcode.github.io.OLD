838feafee67609de9b1a66a50b73fb02bf023a2a
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-05-03 10:58:02 +0200

    b

---
+++ Assets/GetTopBar.js
@@ -0,0 +1,5 @@
+document.write('\
+\
+    <iframe src="https://catwithcode.github.io/Assets/TopBar.html" id="TogpBar" allowtransparency="true" scrolling="no" style="position: relative; width: 100%; height: 190px; border: none"></iframe></iframe>\
+\
+');
