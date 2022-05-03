08999457485f81f1e93ed7672ddcbaa7752c5e57
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-05-03 18:45:24 +0200

    Update GetHead.js

--- Assets/GetHead.js
+++ Assets/GetHead.js
@@ -4,5 +4,6 @@ document.write('\
     <link rel="stylesheet" href="https://catwithcode.github.io/Assets/Style.css">\
     <link rel="icon" type="image/x-icon" href="https://catwithcode.github.io/Assets/favicon.ico">\
     <meta name="description" content="Blog for random computer stuff from my daily life. Girl from Germany. Work as a Software Developer. Programming, Linux, Hacking, Modding and tinkering.">\
+    <meta name="google-site-verification" content="5mzJS7iA70ftCI_MjVxMALYflEVKxquAK-c_jKxFk3o" />\
     \
 ');
