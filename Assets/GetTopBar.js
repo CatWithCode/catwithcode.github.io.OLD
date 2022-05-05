7d66989d7dc26fc89c5dc0e11cf888c438f8aa8d
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-05-05 09:22:54 +0200

    Update GetTopBar.js

--- Assets/GetTopBar.js
+++ Assets/GetTopBar.js
@@ -1,5 +1,17 @@
 document.write('\
-\
-    <iframe src="https://catwithcode.github.io/Assets/TopBar.html" id="TogpBar" allowtransparency="true" scrolling="no" style="position: relative; width: 100%; height: 190px; border: none"></iframe></iframe>\
-\
+    \
+    <h2 align="center">\
+    <a href="https://catwithcode.github.io/"> <img src="https://catwithcode.github.io/Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
+    <a href="https://catwithcode.github.io/">CatWithCode</a>\
+    <br>\
+    <br>\
+    <b>&nbsp;</b>\
+    <a href="https://catwithcode.github.io/blog.html">BLaaaaOG</a>\
+    <b>&nbsp;</b>\
+    <a href="https://catwithcode.github.io/privacy.html">PRIVACY</a>\
+    <b>&nbsp;</b>\
+    <a href="https://catwithcode.github.io/contact.html">CONTACT</a> \
+    <b>&nbsp;</b>\
+    </h2>\
+    \
 ');
