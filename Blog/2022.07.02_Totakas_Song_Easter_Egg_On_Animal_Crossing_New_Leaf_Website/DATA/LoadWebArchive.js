b0ef57d5216de61e3620bd183a8d8d4f9d458bdc
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-07-02 01:30:56 +0200

    New Blog Entry and Button Stuff

---
+++ Blog/2022.07.02_Totakas_Song_Easter_Egg_On_Animal_Crossing_New_Leaf_Website/DATA/LoadWebArchive.js
@@ -0,0 +1,5 @@
+document.write('\
+\
+    <iframe src="https://archive.org/embed/totakas-song-easter-egg-on-animal-crossing-new-leaf-website-lost" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe> \
+    \
+');
