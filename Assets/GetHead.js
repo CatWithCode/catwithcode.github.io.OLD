c6101f5f3bea1ccda086a0605f4dee10299564d3
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2022-11-11 21:08:49 +0100

    Google, Twitter an privacy related updates

--- Assets/GetHead.js
+++ Assets/GetHead.js
@@ -1,6 +1,5 @@
 document.write('\
 \
-    <meta name="google-site-verification" content="5mzJS7iA70ftCI_MjVxMALYflEVKxquAK-c_jKxFk3o" />\
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
     <link rel="stylesheet" href="https://catwithcode.github.io/Assets/Style.css">\
     <link rel="icon" type="image/x-icon" href="https://catwithcode.github.io/Assets/favicon.ico">\
