8bfd49ac671c935563d6560901049138529cccb2
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2023-08-16 22:09:59 +0200

    TESTING

--- Assets/GetHead.js
+++ Assets/GetHead.js
@@ -1,8 +1,8 @@
 document.write('\
 \
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-    <link rel="stylesheet" href="https://catwithcode.github.io/Assets/Style.css">\
-    <link rel="icon" type="image/x-icon" href="https://catwithcode.github.io/Assets/favicon.ico">\
+    <link rel="stylesheet" href="./Assets/Style.css">\
+    <link rel="icon" type="image/x-icon" href="./Assets/favicon.ico">\
     <meta name="description" content="Blog for random computer stuff from my daily life. Girl from Germany. Work as a Software Developer. Programming, Linux, Hacking, Modding and tinkering.">\
     \
 ');
--- Assets/GetTopBar.js
+++ Assets/GetTopBar.js
@@ -1,18 +1,18 @@
 document.write('\
     \
     <h2 align="center">\
-    <a href="https://catwithcode.github.io/"> <img src="https://catwithcode.github.io/Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
-    <a href="https://catwithcode.github.io/">CatWithCode</a>\
+    <a href="./index.html"> <img src="./Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
+    <a href="./index.html">CatWithCode</a>\
     <br>\
     <br>\
     <b>&nbsp;</b>\
-    <a href="https://catwithcode.github.io/blog.html">BLOG</a>\
+    <a href="./blog.html">BLOG</a>\
     <b>&nbsp;</b>\
-    <a href="https://catwithcode.github.io/privacy.html">PRIVACY</a>\
+    <a href="./privacy.html">PRIVACY</a>\
     <b>&nbsp;</b>\
-    <a href="https://catwithcode.github.io/license.html">LICENSE</a>\
+    <a href="./license.html">LICENSE</a>\
     <b>&nbsp;</b>\
-    <a href="https://catwithcode.github.io/contact.html">CONTACT</a> \
+    <a href="./contact.html">CONTACT</a> \
     <b>&nbsp;</b>\
     </h2>\
     <br>\
