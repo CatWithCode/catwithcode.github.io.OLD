36cca7b18878743bfd76b914acec558230d2ce62
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2023-08-16 22:19:28 +0200

    Update GetTopBar.js

--- Assets/GetTopBar.js
+++ Assets/GetTopBar.js
@@ -1,18 +1,18 @@
 document.write('\
     \
     <h2 align="center">\
-    <a href="/index.html"> <img src="./Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
-    <a href="/index.html">CatWithCode</a>\
+    <a href="./index.html"> <img src="./Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
+    <a href="./index.html">CatWithCode</a>\
     <br>\
     <br>\
     <b>&nbsp;</b>\
-    <a href="/blog.html">BLOG</a>\
+    <a href="./blog.html">BLOG</a>\
     <b>&nbsp;</b>\
-    <a href="/privacy.html">PRIVACY</a>\
+    <a href="./privacy.html">PRIVACY</a>\
     <b>&nbsp;</b>\
-    <a href="/license.html">LICENSE</a>\
+    <a href="./license.html">LICENSE</a>\
     <b>&nbsp;</b>\
-    <a href="/contact.html">CONTACT</a> \
+    <a href="./contact.html">CONTACT</a> \
     <b>&nbsp;</b>\
     </h2>\
     <br>\
