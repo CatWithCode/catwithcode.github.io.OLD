9725e3e9ade5e42d3ff11a328b2df7b788725309
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2023-10-07 18:42:26 +0200

    CLEANUP

--- Assets/GetHead.js
+++
@@ -1,8 +0,0 @@
-document.write('\
-\
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-    <link rel="stylesheet" href="/Assets/styles.css">\
-    <link rel="icon" type="image/x-icon" href="/Assets/favicon.ico">\
-    <meta name="description" content="Blog for random computer stuff from my daily life. Girl from Germany. Work as a Software Developer. Programming, Linux, Hacking, Modding and tinkering.">\
-    \
-');
